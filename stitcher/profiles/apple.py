52f9562f8b69bc1839ba5d55446becbc994d4673
George Brighton <george@gebn.co.uk>
2016-04-11 22:14:56 +0100

    Reordered profile parameters.

--- stitcher/profiles/apple.py
+++ stitcher/profiles/apple.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 IPHONE_5S = {
+    'mode': 'RGBA',
     'header_height': 129,
-    'footer_height': 162,
-    'mode': 'RGBA'
+    'footer_height': 162
 }
--- stitcher/profiles/lg.py
+++ stitcher/profiles/lg.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 G3 = {
+    'mode': 'RGBA',
     'header_height': 344,
-    'footer_height': 552,
-    'mode': 'RGBA'
+    'footer_height': 552
 }
